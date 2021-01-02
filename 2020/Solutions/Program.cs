5cdf74dda3649a91dd4425a1b5469d809c0f4618
gabr <arkadiuszgabrys@gmail.com>
2021-01-02 14:58:01 +0100

    2020/24 *

--- 2020/Solutions/Program.cs
+++ 2020/Solutions/Program.cs
@@ -73,8 +73,11 @@ namespace Solutions
             //Console.WriteLine((new Day22()).Solve1(Day22.PUZZLE_INPUT));
             //Console.WriteLine((new Day22()).Solve2(Day22.PUZZLE_INPUT));
 
-            Console.WriteLine((new Day23()).Solve1(Day23.PUZZLE_INPUT, 100));
-            Console.WriteLine((new Day23()).Solve2(Day23.PUZZLE_INPUT, 10000000ul));
+            //Console.WriteLine((new Day23()).Solve1(Day23.PUZZLE_INPUT, 100));
+            //Console.WriteLine((new Day23()).Solve2(Day23.PUZZLE_INPUT, 10000000ul));
+
+            Console.WriteLine((new Day24()).Solve1(Day24.PUZZLE_INPUT));
+            //Console.WriteLine((new Day24()).Solve2(Day24.PUZZLE_INPUT));
         }
     }
 }
---
+++ 2020/Tests/Day24Tests.cs
@@ -0,0 +1,71 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Solutions
+{
+    [TestClass]
+    public class Day24Tests
+    {
+        public static readonly string[] TEST_INPUT =
+        {
+            "sesenwnenenewseeswwswswwnenewsewsw",
+            "neeenesenwnwwswnenewnwwsewnenwseswesw",
+            "seswneswswsenwwnwse",
+            "nwnwneseeswswnenewneswwnewseswneseene",
+            "swweswneswnenwsewnwneneseenw",
+            "eesenwseswswnenwswnwnwsewwnwsene",
+            "sewnenenenesenwsewnenwwwse",
+            "wenwwweseeeweswwwnwwe",
+            "wsweesenenewnwwnwsenewsenwwsesesenwne",
+            "neeswseenwwswnwswswnw",
+            "nenwswwsewswnenenewsenwsenwnesesenew",
+            "enewnwewneswsewnwswenweswnenwsenwsw",
+            "sweneswneswneneenwnewenewwneswswnese",
+            "swwesenesewenwneswnwwneseswwne",
+            "enesenwswwswneneswsenwnewswseenwsese",
+            "wnwnesenesenenwwnenwsewesewsesesew",
+            "nenewswnwewswnenesenwnesewesw",
+            "eneswnwswnwsenenwnwnwwseeswneewsenese",
+            "neswnwewnwnwseenwseesewsenwsweewe",
+            "wseweeenwnesenwwwswnew",
+        };
+
+        [TestMethod]
+        public void Test1FromExamples()
+        {
+            var day = new Day24();
+            Assert.AreEqual(
+                10,
+                day.Solve1(TEST_INPUT));
+        }
+
+        [TestMethod]
+        public void Test1PuzzleInput()
+        {
+            var day = new Day24();
+            Assert.AreEqual(
+                230,
+                day.Solve1(Day24.PUZZLE_INPUT));
+        }
+
+        //[TestMethod]
+        //public void Test2FromExample()
+        //{
+        //    var day = new Day24();
+        //    Assert.AreEqual(
+        //        ,
+        //        day.Solve2(TEST_INPUT));
+        //}
+
+        //[TestMethod]
+        //public void Test2PuzzleInput()
+        //{
+        //    var day = new Day24();
+        //    Assert.AreEqual(
+        //        ,
+        //        day.Solve2(Day24.PUZZLE_INPUT));
+        //}
+
+    }
+}
+
