7b1cb389215e30c0c3e92ca7653f11b4620339ce
gabr <arkadiuszgabrys@gmail.com>
2023-12-05 17:09:58 +0100

    2023 setup

---
+++ 2023/build.zig
@@ -0,0 +1,62 @@
+const std       = @import("std");
+const mem       = std.mem;
+const log       = std.log;
+const fs        = std.fs;
+const debug     = std.debug;
+const Allocator = std.mem.Allocator;
+
+pub fn build(b: *std.Build) void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    // extracted into separate function so I can handle potential errors just in one place
+    addExecutablesFromDirectories(b, allocator) catch |err| {
+        log.err("error while looking for source files: '{s}'", .{ @errorName(err) });
+    };
+}
+
+fn addExecutablesFromDirectories(b: *std.Build, allocator: Allocator) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const test_step = b.step("test", "Run unit tests");
+
+    var cwd = try b.build_root.handle.openDir(".", .{ .iterate = true });
+    defer cwd.close();
+
+    var cwd_it = cwd.iterate();
+    while (try cwd_it.next()) |entry| {
+        // process only directories and skip zig-* special ones
+        if (entry.kind != .directory or mem.startsWith(u8, entry.name, "zig-")) {
+            continue;
+        }
+
+        //log.info("{s}\\", .{ entry.name });
+        var src_dir = try cwd.openDir(entry.name, .{ .iterate = true });
+        defer src_dir.close();
+        var src_dir_it = src_dir.iterate();
+        while (try src_dir_it.next()) |src_entry| {
+            if (src_entry.kind != .file or !mem.endsWith(u8, src_entry.name, ".zig")) {
+                continue;
+            }
+
+            const source_path = try fs.path.join(allocator, &.{ entry.name, src_entry.name });
+            debug.print("source: {s}\n", .{ source_path });
+
+            const exe = b.addExecutable(.{
+                .name = try allocator.dupe(u8, src_entry.name[0..(src_entry.name.len - ".zig".len)]),
+                .root_source_file = .{ .path = source_path },
+                .target = target,
+                .optimize = optimize,
+            });
+            b.installArtifact(exe);
+            const exe_unit_tests = b.addTest(.{
+                .root_source_file = .{ .path = source_path },
+                .target = target,
+                .optimize = optimize,
+            });
+            const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+            test_step.dependOn(&run_exe_unit_tests.step);
+        }
+    }
+}
