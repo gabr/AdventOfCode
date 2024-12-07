e1cd901c973795164e0ea56ee17ab72e9040cecb
arek <arkadiusz.gabrys@optioincentives.no>
2024-12-07 20:58:15 +0100

    2024 04 part1 complete rewrite

--- 2024/04/part1.zig
+++ 2024/04/part1.zig
@@ -7,25 +7,24 @@ pub fn main() !void {
     try stdout.print("{d}\n", .{try solve(stdin)});
 }
 
-const XMASCounter = struct {
-    res: usize = 0,
-    i: usize = 0,
-
-    const XMAS = "XMAS";
-
-    pub fn count(self: *XMASCounter, char: u8) void {
-        if (char != XMAS[self.i]) {
-            self.i = 0;
-            if (char == XMAS[self.i]) self.i += 1;
-            return;
-        }
-        self.i += 1;
-        if (self.i == XMAS.len) {
-            self.i = 0;
-            self.res += 1;
-        }
+fn xmasInDir(rows: [][]const u8, ri: usize, ci: usize, dr: isize, dc: isize) bool {
+    const xmas = "XMAS";
+    var chars: [xmas.len]u8 = undefined;
+    var i: usize = 0;
+    var rid: isize = @intCast(ri);
+    var cid: isize = @intCast(ci);
+    while (true) {
+        chars[i] = rows[@intCast(rid)][@intCast(cid)];
+        i += 1;
+        if (i == xmas.len) break;
+        // advance and check for out of bounds
+        rid += dr;
+        cid += dc;
+        if (rid < 0 or rid >= rows.len or
+            cid < 0 or cid >= rows[0].len) return false;
     }
-};
+    return std.mem.eql(u8, xmas, &chars);
+}
 
 fn solve(reader: anytype) !u64 {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
@@ -39,91 +38,20 @@ fn solve(reader: anytype) !u64 {
         try rows_al.append(line);
     }
     const rows = rows_al.items;
-    var counter = XMASCounter{};
-    for (rows) |row| {
-        for (row) |r| { counter.count(r); }
-        counter.i = 0;
-        for (0..row.len) |i| { counter.count(row[row.len-i-1]); }
-        counter.i = 0;
+    var count: u64 = 0;
+    for (0..rows.len) |ri| {
+        for (0..rows[0].len) |ci| {
+            if (xmasInDir(rows, ri, ci,  0, -1)) count += 1;
+            if (xmasInDir(rows, ri, ci,  0,  1)) count += 1;
+            if (xmasInDir(rows, ri, ci,  1,  0)) count += 1;
+            if (xmasInDir(rows, ri, ci, -1,  0)) count += 1;
+            if (xmasInDir(rows, ri, ci, -1, -1)) count += 1;
+            if (xmasInDir(rows, ri, ci, -1,  1)) count += 1;
+            if (xmasInDir(rows, ri, ci,  1, -1)) count += 1;
+            if (xmasInDir(rows, ri, ci,  1,  1)) count += 1;
+        }
     }
-    for (0..rows[0].len) |ci| {
-        for (0..rows.len) |ri| {
-            counter.count(rows[ri][ci]);
-        }
-        counter.i = 0;
-        for (0..rows.len) |ri| {
-            counter.count(rows[rows.len-ri-1][ci]);
-        }
-        counter.i = 0;
-    }
-    for (0..rows[0].len) |ci| {
-        var x: usize = ci;
-        var y: usize = 0;
-        while (true) {
-            counter.count(rows[y][x]);
-            if (x == 0 or y == rows.len-1) break;
-            x-=1;
-            y+=1;
-        }
-        counter.i = 0;
-        while (true) {
-            counter.count(rows[y][x]);
-            if (x == rows[0].len-1 or y == 0) break;
-            x+=1;
-            y-=1;
-        }
-        counter.i = 0;
-        x = ci;
-        y = 0;
-        while (true) {
-            counter.count(rows[y][x]);
-            if (x == rows[0].len-1 or y == rows.len-1) break;
-            x+=1;
-            y+=1;
-        }
-        counter.i = 0;
-        while (true) {
-            counter.count(rows[y][x]);
-            if (x == 0 or y == 0) break;
-            x-=1;
-            y-=1;
-        }
-        counter.i = 0;
-        if (ci == 0) continue;
-        x = ci;
-        y = rows.len-1;
-        while (true) {
-            counter.count(rows[y][x]);
-            if (x == rows[0].len-1 or y == 0) break;
-            x+=1;
-            y-=1;
-        }
-        counter.i = 0;
-        while (true) {
-            counter.count(rows[y][x]);
-            if (x == 0 or y == rows.len-1) break;
-            x-=1;
-            y+=1;
-        }
-        counter.i = 0;
-        x = rows[0].len-1-ci;
-        y = rows.len-1;
-        while (true) {
-            counter.count(rows[y][x]);
-            if (x == 0 or y == 0) break;
-            x-=1;
-            y-=1;
-        }
-        counter.i = 0;
-        while (true) {
-            counter.count(rows[y][x]);
-            if (x == rows[0].len-1 or y == rows.len-1) break;
-            x+=1;
-            y+=1;
-        }
-        counter.i = 0;
-    }
-    return counter.res;
+    return count;
 }
 
 fn test_solve(expected: u64, input_file_path: []const u8) !void {
