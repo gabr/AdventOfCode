1cef740dfa6f2646d539ec19ab4ec02aa4397bbe
gabr <arkadiuszgabrys@gmail.com>
2024-12-18 09:14:37 +0100

    2024 17 **

--- 2024/17/part2.zig
+++ 2024/17/part2.zig
@@ -32,7 +32,26 @@ const CPU = struct {
     ip: usize,
     mem: [] u8,
 
-    pub fn step(self: *CPU) !?u64 {
+    pub fn simulateFromStartTillHalt(self: *CPU, rega: u64, out_buf: []u8) ![]u8 {
+        self.a = rega;
+        self.b = 0;
+        self.c = 0;
+        self.ip = 0;
+        var outi: usize = 0;
+        while (true) {
+            const output_op = self.step() catch |err| switch (err) {
+                error.Halt => break,
+                else => return err,
+            };
+            if (output_op) |output| {
+                out_buf[outi] = output;
+                outi += 1;
+            }
+        }
+        return out_buf[0..outi];
+    }
+
+    pub fn step(self: *CPU) !?u8 {
         if (self.ip == self.mem.len) return error.Halt;
         const opcode = self.mem[self.ip];
         self.ip+=1;
@@ -110,9 +129,9 @@ const CPU = struct {
         self.b = res;
     }
 
-    fn out(self: CPU, operand: u8) !?u64 {
+    fn out(self: CPU, operand: u8) !?u8 {
         const opval = try self.comboval(operand);
-        const res = opval & 0b111;
+        const res: u8 = @intCast(opval & 0b111);
         //dprint("out({d}): {d} & 0b111 = {d}\n", .{operand, opval, res});
         return res;
     }
@@ -134,98 +153,92 @@ const CPU = struct {
     }
 };
 
-
-fn findStart(init_cpu: CPU) !u64 {
-    var count_output: usize = 0;
-    var i: u64 = 10;
-    while (true) {
-        var cpu = CPU {
-            .a   = i,
-            .b   = init_cpu.b,
-            .c   = init_cpu.c,
-            .ip  = 0,
-            .mem = init_cpu.mem,
-        };
-        count_output = 0;
+fn search(inita: u64, cpu: *CPU, testi: usize, good_bits: u6, out_buf: []u8) !?u64 {
+    const basea = inita << good_bits;
+    //for (0..testi) |_| { dprint(" ", .{}); }
+    //dprint("search(inita: {d}, ..., testi: {d}, good_bits: {d}, ...) basea: {d} ({b})\n", .{
+    //    inita, testi, good_bits, basea, basea });
+    for (0..std.math.maxInt(u4)+1) |i| {
+        const a = basea+i;
+        const output = try cpu.simulateFromStartTillHalt(a, out_buf);
+        var outputi: usize = output.len-1;
+        var memi:    usize = cpu.mem.len-1;
+        var good:    usize = 0;
         while (true) {
-            const output_op = cpu.step() catch |err| switch (err) {
-                error.Halt => break,
-                else => return err,
-            };
-            if (output_op) |_| { count_output += 1; }
+            if (output[outputi] != cpu.mem[memi]) break;
+            good += 1;
+            if (outputi == 0) break; outputi -= 1;
+            if (memi    == 0) break; memi    -= 1;
         }
-        if (count_output == init_cpu.mem.len) return i;
-        i = i << 1;
+        if (good == cpu.mem.len) return a;
+        if (good_bits >= @as(u6, @intCast(cpu.mem.len*3))) continue;
+        if (good >= testi) {
+            if (try search(a, cpu, testi+1, good_bits+3, out_buf)) |found| {
+                return found;
+            }
+        }
     }
-    unreachable;
+    return null;
+}
+
+fn search2(inita: u64, cpu: *CPU, gcount: usize, out_buf: []u8) !?u64 {
+    for (0..gcount*8) |i| {
+        const a = inita + i;
+        const output = try cpu.simulateFromStartTillHalt(a, out_buf);
+        //for (0..gcount) |_| { dprint(" ", .{}); }
+        //dprint("search(inita: {d}, ..., gcount: {d}, ...) a: {d} ({b})\n", .{ inita, gcount, a, a });
+        var outputi: usize = output.len-1;
+        var memi:    usize = cpu.mem.len-1;
+        var good:    usize = 0;
+        while (true) {
+            if (output[outputi] != cpu.mem[memi]) break;
+            good += 1;
+            if (outputi == 0) break; outputi -= 1;
+            if (memi    == 0) break; memi    -= 1;
+        }
+        if (good == cpu.mem.len) return a;
+        if (good >= gcount) {
+            if (try search2(a*8, cpu, gcount+1, out_buf)) |found| {
+                return found;
+            }
+        }
+    }
+    return null;
+}
+
+fn bf(cpu: *CPU, out_buf: []u8) !?u64 {
+    var a: u64 = 0;
+    while (true) {
+        const output = try cpu.simulateFromStartTillHalt(a, out_buf);
+        if (output[output.len-1] == cpu.mem[cpu.mem.len-1]) {
+            dprint("{d:10} ({b:_>40}): {any}\n", .{a, a, output});
+        }
+        a += 1;
+    }
 }
 
 fn solve(reader: anytype) !u64 {
     const input = try reader.readAllAlloc(gpa(), std.math.maxInt(usize));
     var lines_it = mem.splitScalar(u8, input, '\n');
     //dprint("debug:\n", .{});
-             _ = try parseInt(u64, lines_it.next().?["Register A: ".len..]);
+    const rega = try parseInt(u64, lines_it.next().?["Register A: ".len..]);
     const regb = try parseInt(u64, lines_it.next().?["Register B: ".len..]);
     const regc = try parseInt(u64, lines_it.next().?["Register C: ".len..]);
     _ = lines_it.next().?;
     const mem_str = lines_it.next().?["Program: ".len..];
     var mem_it = mem.splitScalar(u8, mem_str, ',');
     var mem_buf = try gpa().alloc(u8, (mem_str.len/2)+1);
-    var out_buf = try gpa().alloc(u64, (mem_str.len/2)+1);
     for (0..mem_buf.len) |i| { mem_buf[i] = try parseInt(u8, mem_it.next().?); }
-    var inita: u64 = try findStart(.{
-        .a   = 0,
+    var cpu = CPU {
+        .a   = rega,
         .b   = regb,
         .c   = regc,
         .ip  = 0,
         .mem = mem_buf,
-    });
-    dprint("start at : {d} (b{b})\n", .{inita, inita});
-    var memi: usize = 0;
-    var memi_max: usize = 0;
-    while (true) {
-        //dprint("testing a: {d}", .{inita});
-        memi = 0;
-        var cpu = CPU {
-            .a   = inita,
-            .b   = regb,
-            .c   = regc,
-            .ip  = 0,
-            .mem = mem_buf,
-        };
-        //dprint("intial state:\n", .{});
-        //dprint(" reg a: {d}\n",   .{inita});
-        //dprint(" reg b: {d}\n",   .{regb});
-        //dprint(" reg c: {d}\n",   .{regc});
-        //dprint("    ip: {d}\n",   .{0});
-        //dprint(" mem: {any}\n",   .{mem_buf});
-        while (true) {
-            const output_op = cpu.step() catch |err| switch (err) {
-                error.Halt => {
-                    if (memi == mem_buf.len) return inita;
-                    break;
-                },
-                else => return err,
-            };
-            if (output_op) |output| {
-                out_buf[memi] = output;
-                if (output != mem_buf[memi]) {
-                    if (memi >= memi_max) {
-                        dprint("inita: {d: >16} (b{b:_>64}), memi: {d}, mem: {any}\n", .{inita, inita, memi, out_buf[0..memi]});
-                        memi_max = memi;
-                    }
-                    break;
-                }
-                memi += 1;
-                if (memi == mem_buf.len) {
-                    dprint("inita: {d: >16} (b{b:_>64}), memi: {d}, mem: {any}\n", .{inita, inita, memi, out_buf[0..memi]});
-                    return inita;
-                }
-            }
-        }
-        inita += 1;
-    }
-    unreachable;
+    };
+    var out_buf: [255]u8 = undefined;
+    return try search2(0, &cpu, 1, &out_buf) orelse return error.NotFound;
+    //return try bf(&cpu, &out_buf) orelse return error.NotFound;
 }
 
 fn test_solve(expected: u64, input_file_path: []const u8) !void {
@@ -233,5 +246,5 @@ fn test_solve(expected: u64, input_file_path: []const u8) !void {
     defer file.close();
     try std.testing.expectEqual(expected, try solve(file.reader()));
 }
-test "example2" { try test_solve(117440, "./17/example2.txt"); }
-//test "input"    { try test_solve(0, "./17/input.txt"); }
+test "example2" { try test_solve(117440,          "./17/example2.txt"); }
+test "input"    { try test_solve(202991746427434, "./17/input.txt"); }
